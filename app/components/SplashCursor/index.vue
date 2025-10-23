dcb37acc957965d407d50c29b92543974a445e31
chendada00 <53457984+chendada00@users.noreply.github.com>
2025-10-23 10:51:04 +0800

    减弱鼠标特效

--- app/components/SplashCursor/index.vue
+++ app/components/SplashCursor/index.vue
@@ -53,7 +53,7 @@ const props = withDefaults(defineProps<SplashCursorProps>(), {
   PRESSURE: 0.1,
   PRESSURE_ITERATIONS: 20,
   CURL: 3,
-  SPLAT_RADIUS: 0.06,
+  SPLAT_RADIUS: 0.1,
   SPLAT_FORCE: 6000,
   SHADING: true,
   COLOR_UPDATE_SPEED: 10,
