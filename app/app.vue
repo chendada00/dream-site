8fff5fe99b598d76f4a39770c2db1bf2af8d6a25
chendada00 <53457984+chendada00@users.noreply.github.com>
2025-10-23 10:55:15 +0800

    减弱特效1

--- app/app.vue
+++ app/app.vue
@@ -19,7 +19,7 @@
       :PRESSURE="0.1"
       :PRESSURE_ITERATIONS="20"
       :CURL="3"
-      :SPLAT_RADIUS="0.2"
+      :SPLAT_RADIUS="0.1"
       :SPLAT_FORCE="6000"
       :SHADING="true"
       :COLOR_UPDATE_SPEED="10"
