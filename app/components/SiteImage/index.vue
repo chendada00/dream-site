00192ce9f8488bf346873caef11fd42331a267c5
陈波 <2207117672@qq.com>
2026-08-30 17:13:07 +0800

    修复报错

--- app/components/SiteImage/index.vue
+++ app/components/SiteImage/index.vue
@@ -1,24 +1,39 @@
 <template>
-  <NuxtImg v-if="isValidImageUrl(url)" :src="url" alt="logo" :width="size" :height="size" />
-  <UIcon v-else :name="url" :size="String(size)" :style="{ color }" />
+  <NuxtImg
+      v-if="url && isValidImageUrl(url)"
+      :src="url"
+      alt="logo"
+      :width="size"
+      :height="size"
+  />
+
+  <UIcon
+      v-else-if="url"
+      :name="url"
+      :size="String(size)"
+      :style="{ color }"
+  />
+
 </template>
 
 <script setup lang="ts">
 type Props = {
-  url: string; // 图标
-  size?: number; // 图片大小，宽高同比
-  color?: string; // 颜色
+  url?: string | null; // 修改类型约束，真实反映运行时可能传入 null 的情况
+  size?: number;
+  color?: string;
 };
 
-// 父组件传递参数
 withDefaults(defineProps<Props>(), {
+  url: '',
   size: 40,
   color: "",
 });
 
-// 判断地址是否是合法的图片地址
-const isValidImageUrl = (url: string) => {
-  const urlPattern = /^(https?:\/\/)[\da-z.-]+\.([a-z.]{2,6})([/\w .-]*\.(jpg|jpeg|png|gif|svg|ico|webp))$/i;
-  return urlPattern.test(url);
+// 安全校验图片地址
+const isValidImageUrl = (urlStr: string) => {
+  if (!urlStr) return false;
+  // 提示：现有的正则对于相对路径（如 /images/logo.png）或带 Query 参数的 URL（如 img.png?v=1）会返回 false
+  const urlPattern = /^(https?:\/\/)[\da-z.-]+\.([a-z.]{2,6})([/\w .-]*\.(jpg|jpeg|png|gif|svg|ico|webp))/i;
+  return urlPattern.test(urlStr);
 };
 </script>
